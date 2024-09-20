139b8f6236a650d5503a8b0f4b26de0926e8ab84
Jaspreet Singh <js9316713287@gmail.com>
2024-09-20 17:34:12 +0530

    New Changes

--- Controller/Admin.js
+++ Controller/Admin.js
@@ -126,7 +126,7 @@ const loginAdmin = async (req, res) => {
       expires : expiresIn,
       httpOnly : false,
       secure: process.env.NODE_ENV === "production",
-      sameSite : "Strict",
+      sameSite : "None",
       path : '/'
     });
 
